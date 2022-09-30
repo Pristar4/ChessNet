5d988c6697314fb44856056a908bca4613c4a3d5
Felix Jung <felix.j.jung@gmail.com>
2022-09-30 22:05:28 +0200

    added stuff

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import chessnet.engineInfo
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println(engineInfo())
+
+    // get version from build.gradle.kts
+
+    /*
+    Uci.init(Options)
+
+
+
+    Uci.loop(argc,argv)
+*/
+
 }
+
+
+
+
